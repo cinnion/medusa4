bd088ed24723ebe4179f70cad5505e3336f4c132
Douglas Needham <douglas.w.needham@gmail.com>
2025-08-24 19:02:04 -0400

    Add initial migration, seeder and test classes for Branch model.

---
+++ database/migrations/2025_08_24_223208_create_branch_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use MongoDB\Laravel\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('branch', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('branch', 10)->unique();
+            $table->string('branch_name', 100);
+            $table->json('equivalent');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('branch');
+    }
+};
