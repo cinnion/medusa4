73c0d3a0c50a0a3a2c09600445b11628fda5fa86
Douglas Needham <douglas.w.needham@gmail.com>
2025-08-24 23:16:53 -0400

    Add V2 Audit model with template migration/seeder.

---
+++ app/Models/Audit.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+
+class Audit extends Eloquent
+{
+    protected $fillable = ['member_id', 'action', 'collection_name', 'document_id', 'document_values', 'from_where'];
+
+    protected $table = 'audit_trail';
+}
