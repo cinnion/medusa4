124daef193480c72e99b34596c8d9b3edcacce03
Douglas Needham <douglas.w.needham@gmail.com>
2025-08-26 16:28:33 -0400

    Enhance tests for _getAwards()
    - Rename original test to a more meaningful name.
    - Add assertions on original test.
    - Add second test to get multiple awards/groups mixed together.

--- tests/Unit/AwardTest.php
+++ tests/Unit/AwardTest.php
@@ -11,7 +11,7 @@ class AwardTest extends TestCase
 {
     use DatabaseMigrations;
 
-    public function testGetAwards(): void
+    public function testGetAwardsJustQEExpectGrouped(): void
     {
         // Arrange
         $this->seed(AwardSeeder::class);
@@ -21,7 +21,11 @@ class AwardTest extends TestCase
 
         // Assert
         $this->assertIsArray($awards);
+        $this->assertCount(1, $awards); // Only one group expected
+        $this->assertArrayHasKey(0, $awards);
+        $this->assertArrayHasKey('group', $awards[0]);
         $this->count(5, $awards[0]['group']['awards']);
+        $this->assertEquals('Most Regal Order of Queen Elizabeth', $awards[0]['group']['label']);
         $this->assertInstanceOf(Award::class, $awards[0]['group']['awards'][0]);
         $this->assertInstanceOf(Award::class, $awards[0]['group']['awards'][1]);
         $this->assertInstanceOf(Award::class, $awards[0]['group']['awards'][2]);
@@ -32,7 +36,47 @@ class AwardTest extends TestCase
         $this->assertEquals('OE',  $awards[0]['group']['awards'][2]->code);
         $this->assertEquals('ME',  $awards[0]['group']['awards'][3]->code);
         $this->assertEquals('EM',  $awards[0]['group']['awards'][4]->code);
+        $this->assertFalse($awards[0]['group']['multiple']);
     }
+
+    public function testGetAwardsWithMultipleAwardsWithUnknownExpectedResults(): void
+    {
+        // Arrange
+        $this->seed(AwardSeeder::class);
+
+        // Act
+        $awards = Award::_getAwards('L', ['HWC', 'PMV', 'CE', 'KE', 'ME', 'OE', 'EM', 'MtSM', 'GCM', 'ZZ']);
+
+        // Assert
+        $this->assertIsArray($awards);
+        $this->assertCount(4, $awards); // Three groups expected
+        $this->assertInstanceOf(Award::class, $awards[0]);
+        $this->assertEquals('PMV', $awards[0]->code, 'First award should be PMV');
+        $this->count(5, $awards[1]['group']['awards']);
+        $this->assertEquals('Most Regal Order of Queen Elizabeth', $awards[1]['group']['label']);
+        $this->assertInstanceOf(Award::class, $awards[1]['group']['awards'][0]);
+        $this->assertInstanceOf(Award::class, $awards[1]['group']['awards'][1]);
+        $this->assertInstanceOf(Award::class, $awards[1]['group']['awards'][2]);
+        $this->assertInstanceOf(Award::class, $awards[1]['group']['awards'][3]);
+        $this->assertInstanceOf(Award::class, $awards[1]['group']['awards'][4]);
+        $this->assertEquals('KE',  $awards[1]['group']['awards'][0]->code);
+        $this->assertEquals('CE',  $awards[1]['group']['awards'][1]->code);
+        $this->assertEquals('OE',  $awards[1]['group']['awards'][2]->code);
+        $this->assertEquals('ME',  $awards[1]['group']['awards'][3]->code);
+        $this->assertEquals('EM',  $awards[1]['group']['awards'][4]->code);
+        $this->assertFalse($awards[1]['group']['multiple']);
+        $this->assertInstanceOf(Award::class, $awards[2]);
+        $this->assertEquals('HWC', $awards[2]->code, 'Third award should be HWC');
+        $this->assertIsArray($awards[3]['group']['awards']);
+        $this->assertCount(2, $awards[3]['group']['awards']);
+        $this->assertInstanceOf(Award::class, $awards[3]['group']['awards'][0]);
+        $this->assertEquals('MtSM',  $awards[3]['group']['awards'][0]->code);
+        $this->assertInstanceOf(Award::class, $awards[3]['group']['awards'][1]);
+        $this->assertEquals('GCM',  $awards[3]['group']['awards'][1]->code);
+        $this->assertEquals('Length of Service Awards', $awards[3]['group']['label']);
+        $this->assertTrue($awards[3]['group']['multiple']);
+    }
+
 
     public function testGetDisplayOrderKnownAwardExpect29(): void
     {
