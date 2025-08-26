4999d1529336aacc0cc0389677be235078dae3c1
Douglas Needham <douglas.w.needham@gmail.com>
2025-08-26 18:42:33 -0400

    Add Billet model from V2
    - Add the model from V2, with only namespace/use statement changes.

--- app/Models/Billet.php
+++ app/Models/Billet.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\Model;
 
 class Billet extends Model
 {
-    //
+    protected $fillable = ['billet_name'];
+
+    public static $rules = ['billet_name' => 'required|unique:billets'];
+
+    public static function getBillets()
+    {
+        $results = self::all();
+        $billets = [];
+
+        foreach ($results as $billet) {
+            $billets[$billet->billet_name] = $billet->billet_name;
+        }
+
+        asort($billets, SORT_NATURAL);
+
+        return $billets;
+    }
+
+    public function getAssignedCount()
+    {
+        return User::where('assignment.billet', '=', $this->billet_name)->count();
+    }
 }
