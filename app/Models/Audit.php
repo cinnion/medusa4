c0e31624c13404c9d45da3f44b42e94eb5a5ed0d
Douglas Needham <douglas.w.needham@gmail.com>
2025-08-25 20:22:47 -0400

    Add properties to Audit model docblock.

--- app/Models/Audit.php
+++ app/Models/Audit.php
@@ -4,6 +4,18 @@ namespace App\Models;
 
 use MongoDB\Laravel\Eloquent\Model as Model;
 
+/**
+ * Class Audit.
+ *
+ * @property string member_id
+ * @property string action
+ * @property string collection_name
+ * @property string document_id
+ * @property array document_values
+ * @property string from_where
+ * @property \MongoDB\BSON\UTCDateTime created_at
+ * @property \MongoDB\BSON\UTCDateTime updated_at
+ */
 class Audit extends Model
 {
     /***
