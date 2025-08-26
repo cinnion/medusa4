04d7903310447e2f021c8511bdc6e22e5c361f68
Douglas Needham <douglas.w.needham@gmail.com>
2025-08-26 19:21:42 -0400

    Place getBillets() under test.

--- tests/Unit/BilletTest.php
+++ tests/Unit/BilletTest.php
@@ -2,9 +2,28 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Billet;
+use Database\Seeders\BilletSeeder;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
 
 class BilletTest extends TestCase
 {
- 
+    use DatabaseMigrations;
+
+    public function testGetBilletsExpectedResults()
+    {
+        // Arrange
+        $this->seed(BilletSeeder::class);
+        $expected = [
+            'Fourth Space Lord' => 'Fourth Space Lord',
+            'President' => 'President',
+        ];
+
+        // Act
+        $billets = Billet::getBillets();
+
+        // Assert
+        $this->assertEquals($expected, $billets);
+    }
 }
