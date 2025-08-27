fd21f7a868bf6414b5d8b63b001c5c6fb3d5ba00
Douglas Needham <douglas.w.needham@gmail.com>
2025-08-26 20:52:18 -0400

    Stage stub model, migration, seeder and tests for Chapter model.

---
+++ database/seeders/ChapterSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ChapterSeeder extends Seeder
+{
+    use DatabaseMigrations;
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
