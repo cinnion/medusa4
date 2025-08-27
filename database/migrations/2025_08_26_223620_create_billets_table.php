e2bc95380869d172ea8dfe338b6aec96bfb27d23
Douglas Needham <douglas.w.needham@gmail.com>
2025-08-26 21:58:32 -0400

    Fix typo in billet migration

--- database/migrations/2025_08_26_223620_create_billets_table.php
+++ database/migrations/2025_08_26_223620_create_billets_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
 
-            $this->string('billet_name')->unique();
+            $table->string('billet_name')->unique();
         });
     }
 
