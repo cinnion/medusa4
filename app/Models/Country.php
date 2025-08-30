c8780e26c86daeb28c56f37b1581c1b9f51f2328
Douglas Needham <douglas.w.needham@gmail.com>
2025-08-30 17:36:10 -0400

    Bring over Country model.
    - Require webpatser/laravel-countries, which is used by the model.
    - Bring over the Country model from v2 and update uses.
    - Create minimal seeder.
    - Create migration

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -3,8 +3,28 @@
 namespace App\Models;
 
 use MongoDB\Laravel\Eloquent\Model;
+use Webpatser\Countries\Countries;
 
 class Country extends Model
 {
-    //
+    protected $fillable = [];
+
+    public $timestamps = false;
+
+    public static function getCountries()
+    {
+        $countryLister = new Countries();
+        $results = $countryLister->getList();
+        $countries = [];
+
+        foreach ($results as $country) {
+            $countries[$country['iso_3166_3']] = $country['name'];
+        }
+
+        asort($countries);
+
+        $countries = ['' => 'Select a Country'] + $countries;
+
+        return $countries;
+    }
 }
