a16365b793be010b12ab1b8ac9dafc25851a5365
Douglas Needham <douglas.w.needham@gmail.com>
2025-08-30 18:12:28 -0400

    Get Country::getCountries() under test.
    - Get method under test.
    - Remove migration and seeder, as they are not needed.
    
    NOTE: webpatser/laravel-countries uses a JSON file for its data.

--- tests/Unit/CountryTest.php
+++ tests/Unit/CountryTest.php
@@ -2,17 +2,28 @@
 
 namespace Tests\Unit;
 
+use App\Models\Country;
+use Database\Seeders\CountrySeeder;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
 class CountryTest extends TestCase
 {
     use DatabaseMigrations;
 
-    public function setUp(): void
+    public function testGetCountriesExpectedReturn (): void
     {
-        parent::setUp();
+        // Arrange
+        $ca = Country::where('name', 'Canada')->first();
+        $us = Country::where('name', 'United States')->first();
 
-        $this->seed(CountrySeeder::class);
+        // Act
+        $countries = Country::getCountries();
+
+        // Assert
+        $this->assertIsArray($countries);
+        $this->assertCount(250, $countries);
+        $this->assertEquals('Canada', $countries['CAN']);
+        $this->assertEquals('United States', $countries['USA']);
     }
-
 }
