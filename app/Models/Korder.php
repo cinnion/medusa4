25f5dbe1c684f42835aebf08805621aaaefa1a20
Douglas Needham <douglas.w.needham@gmail.com>
2025-08-30 23:14:37 -0400

    Copy over Korders from v2.

--- app/Models/Korder.php
+++ app/Models/Korder.php
@@ -6,5 +6,40 @@ use MongoDB\Laravel\Eloquent\Model;
 
 class Korder extends Model
 {
-    //
+    protected $fillable = [
+        'order',
+        'filename',
+        'classes'
+    ];
+
+    protected $table = 'korders';
+
+    public function getPrecedence($options)
+    {
+        if (is_array($options) === false) {
+            $options = [
+                'type' => 'postnominal',
+                'value' => $options,
+            ];
+        }
+
+        foreach ($this->classes as $item) {
+            if ($item[$options['type']] == $options['value']) {
+                return $item['precedence'];
+            }
+        }
+
+        return false;
+    }
+
+    public function getClassName($postNominal)
+    {
+        foreach ($this->classes as $item) {
+            if ($item['postnominal'] == $postNominal) {
+                return $item['class'];
+            }
+        }
+
+        return false;
+    }
 }
