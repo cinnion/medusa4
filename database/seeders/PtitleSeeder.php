dccf61601a3d1a58262f3f2390048a9c6b6102f9
Douglas Needham <douglas.w.needham@gmail.com>
2025-08-31 19:02:06 -0400

    Add stub Ptile model, migration and seeder.

---
+++ database/seeders/PtitleSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Permission;
+use Illuminate\Database\Seeder;
+
+class PtitleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
