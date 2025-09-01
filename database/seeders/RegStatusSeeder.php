c1134a2789cbe4b12d37c6ca391cb40e5c11038a
Douglas Needham <douglas.w.needham@gmail.com>
2025-08-31 21:39:15 -0400

    Add seeder for prod data for RegStatus

--- database/seeders/RegStatusSeeder.php
+++ database/seeders/RegStatusSeeder.php
@@ -12,6 +12,48 @@ class RegStatusSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $statuses = [
+            [
+                'status' => 'Active',
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b4bc2',
+            ],
+            [
+                'status' => 'Inactive',
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b4bc4',
+            ],
+            [
+                'status' => 'Suspended',
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b4bc8',
+            ],
+            [
+                'status' => 'Pending',
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b4bc6',
+            ],
+            [
+                'status' => 'Expelled',
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b4bca',
+            ],
+        ];
+
+        foreach ($statuses as $regstatus) {
+            $rec = new RegStatus($regstatus);
+            $rec->created_at = $regstatus['created_at'];
+            $rec->updated_at = $regstatus['updated_at'];
+            if (isset($regstatus['deleted'])) {
+                $rec->deleted_at = $regstatus['deleted_at'];
+            }
+            $rec->timestamps = false;
+            $rec->save();
+        }
     }
 }
