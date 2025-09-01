e43bd657a97ea3a2e498c677d71b5a0487119290
Douglas Needham <douglas.w.needham@gmail.com>
2025-08-31 21:24:25 -0400

    Copy over v2 RegStatus contents and update migration.

--- app/Models/RegStatus.php
+++ app/Models/RegStatus.php
@@ -6,5 +6,22 @@ use MongoDB\Laravel\Eloquent\Model;
 
 class RegStatus extends Model
 {
+    protected $fillable = [
+        'status',
+    ];
+
     protected $table = 'status';
+
+    public static function getRegStatuses()
+    {
+        $retVal = [];
+
+        foreach (self::orderBy('status')->get() as $status) {
+            $retVal[$status->status] = $status->status;
+        }
+
+        asort($retVal);
+
+        return ['' => 'Select a status'] + $retVal;
+    }
 }
--- database/migrations/2025_09_01_010710_create_reg_statuses_table.php
+++ database/migrations/2025_09_01_010710_create_reg_statuses_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             Schema::create('status', function (Blueprint $table) {
                 $table->id();
                 $table->timestamps();
+
+                $table->string('status');
             });
         }
     }
