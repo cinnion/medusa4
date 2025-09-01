50d95d6fbf91ad18c0662d3261b99f976f2808e5
Douglas Needham <douglas.w.needham@gmail.com>
2025-08-31 21:10:11 -0400

    Add stub RegStatus model, migration and seeder.

---
+++ database/seeders/RegStatusSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\RegStatus;
+use Illuminate\Database\Seeder;
+
+class RegStatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
