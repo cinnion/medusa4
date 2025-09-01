a8412e5bb049d5f04cc03623164dfa0b116f8b0c
Douglas Needham <douglas.w.needham@gmail.com>
2025-09-01 13:30:46 -0400

    Add properties to the Report model.

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -4,21 +4,41 @@ namespace App\Models;
 
 use MongoDB\Laravel\Eloquent\Model;
 
+/**
+ * @property id
+ * @property updated_at
+ *
+ * @property string _method
+ * @property string _token
+ * @property string activities
+ * @property string award_actions
+ * @property string chapter_id
+ * @property array chapter_info
+ * @property array command_crew
+ * @property string courses
+ * @property array new_crew
+ * @property string problems
+ * @property string promotion_actions
+ * @property string questions
+ * @property string report_date
+ * @property date report_sent
+ * @property string send_report
+ */
 class Report extends Model
 {
     protected $fillable = [
-        'promotion_actions',
-        'award_actions',
-        'courses',
         'activities',
-        'problems',
-        'questions',
-        'report_sent',
-        'command_crew',
-        'new_crew',
+        'award_actions',
         'chapter_id',
         'chapter_info',
+        'command_crew',
+        'courses',
+        'new_crew',
+        'problems',
+        'promotion_actions',
+        'questions',
         'report_date',
+        'report_sent',
     ];
 
     /**
