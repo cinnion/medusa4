6496e62a8035b8e87f42efcc903459a7a0db1e78
Douglas Needham <douglas.w.needham@gmail.com>
2025-09-01 20:11:50 -0400

    Add production data seeder.

---
+++ database/seeders/TigSeeder.php
@@ -0,0 +1,439 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tig;
+use Illuminate\Database\Seeder;
+
+class TigSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $tigs = [
+            [
+                'grade' => 'E-6',
+                'tig' => [
+                        'tig_as' => 'E-5',
+                        'tig' => '11',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48aa',
+            ],
+            [
+                'grade' => 'F-3',
+                'tig' => [
+                        'tig_as' => 'F-2',
+                        'tig' => '36',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48d0',
+            ],
+            [
+                'grade' => 'WO-3',
+                'tig' => [
+                        'tig_as' => 'WO-2',
+                        'tig' => '11',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48b8',
+            ],
+            [
+                'grade' => 'C-13',
+                'tig' => [
+                        'tig_as' => 'C-12',
+                        'tig' => '18',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48ea',
+            ],
+            [
+                'grade' => 'E-3',
+                'tig' => [
+                        'tig_as' => 'E-2',
+                        'tig' => '5',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48a4',
+            ],
+            [
+                'grade' => 'C-4',
+                'tig' => [
+                        'tig_as' => 'C-3',
+                        'tig' => '15',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48d8',
+            ],
+            [
+                'grade' => 'C-9',
+                'tig' => [
+                        'tig_as' => 'C-5',
+                        'tig' => '9',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48e2',
+            ],
+            [
+                'grade' => 'O-3',
+                'tig' => [
+                        'tig_as' => 'O-2',
+                        'tig' => '9',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48c2',
+            ],
+            [
+                'grade' => 'F-1',
+                'tig' => [
+                        'tig_as' => 'O-6-B',
+                        'tig' => '18',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48cc',
+            ],
+            [
+                'grade' => 'F-4',
+                'tig' => [
+                        'tig_as' => 'F-3',
+                        'tig' => '48',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48d2',
+            ],
+            [
+                'grade' => 'E-10',
+                'tig' => [
+                        'tig_as' => 'E-9',
+                        'tig' => '24',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48b2',
+            ],
+            [
+                'grade' => 'WO-5',
+                'tig' => [
+                        'tig_as' => 'WO-4',
+                        'tig' => '15',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48bc',
+            ],
+            [
+                'grade' => 'C-8',
+                'tig' => [
+                        'tig_as' => 'C-5',
+                        'tig' => '6',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48e0',
+            ],
+            [
+                'grade' => 'O-6-A',
+                'tig' => [
+                        'tig_as' => 'O-5',
+                        'tig' => '18',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48c8',
+            ],
+            [
+                'grade' => 'C-14',
+                'tig' => [
+                        'tig_as' => 'C-13',
+                        'tig' => '20',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48ec',
+            ],
+            [
+                'grade' => 'C-11',
+                'tig' => [
+                        'tig_as' => 'C-10',
+                        'tig' => '12',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48e6',
+            ],
+            [
+                'grade' => 'E-7',
+                'tig' => [
+                        'tig_as' => 'E-6',
+                        'tig' => '13',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48ac',
+            ],
+            [
+                'grade' => 'C-7',
+                'tig' => [
+                        'tig_as' => 'C-6',
+                        'tig' => '24',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48de',
+            ],
+            [
+                'grade' => 'C-5',
+                'tig' => [
+                        'tig_as' => 'C-4',
+                        'tig' => '24',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48da',
+            ],
+            [
+                'grade' => 'C-6',
+                'tig' => [
+                        'tig_as' => 'C-5',
+                        'tig' => '24',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48dc',
+            ],
+            [
+                'grade' => 'E-5',
+                'tig' => [
+                        'tig_as' => 'E-4',
+                        'tig' => '9',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48a8',
+            ],
+            [
+                'grade' => 'O-2',
+                'tig' => [
+                        'tig_as' => 'O-1',
+                        'tig' => '6',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48c0',
+            ],
+            [
+                'grade' => 'C-15',
+                'tig' => [
+                        'tig_as' => 'C-14',
+                        'tig' => '20',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48ee',
+            ],
+            [
+                'grade' => 'WO-2',
+                'tig' => [
+                        'tig_as' => 'WO-1',
+                        'tig' => '9',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48b6',
+            ],
+            [
+                'grade' => 'O-5',
+                'tig' => [
+                        'tig_as' => 'O-4',
+                        'tig' => '15',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48c6',
+            ],
+            [
+                'grade' => 'O-4',
+                'tig' => [
+                        'tig_as' => 'O-3',
+                        'tig' => '12',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48c4',
+            ],
+            [
+                'grade' => 'C-16',
+                'tig' => [
+                        'tig_as' => 'C-15',
+                        'tig' => '20',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48f0',
+            ],
+            [
+                'grade' => 'E-4',
+                'tig' => [
+                        'tig_as' => 'E-3',
+                        'tig' => '7',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48a6',
+            ],
+            [
+                'grade' => 'WO-4',
+                'tig' => [
+                        'tig_as' => 'WO-3',
+                        'tig' => '13',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48ba',
+            ],
+            [
+                'grade' => 'C-2',
+                'tig' => [
+                        'tig_as' => 'C-1',
+                        'tig' => '3',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48d4',
+            ],
+            [
+                'grade' => 'C-3',
+                'tig' => [
+                        'tig_as' => 'C-2',
+                        'tig' => '5',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48d6',
+            ],
+            [
+                'grade' => 'C-10',
+                'tig' => [
+                        'tig_as' => 'C-9',
+                        'tig' => '12',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48e4',
+            ],
+            [
+                'grade' => 'O-1',
+                'tig' => [
+                        'tig_as' => 'E-1',
+                        'tig' => '6',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48be',
+            ],
+            [
+                'grade' => 'O-6-B',
+                'tig' => [
+                        'tig_as' => 'O-6-A',
+                        'tig' => '18',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48ca',
+            ],
+            [
+                'grade' => 'E-8',
+                'tig' => [
+                        'tig_as' => 'E-7',
+                        'tig' => '15',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48ae',
+            ],
+            [
+                'grade' => 'E-9',
+                'tig' => [
+                        'tig_as' => 'E-8',
+                        'tig' => '19',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48b0',
+            ],
+            [
+                'grade' => 'C-17',
+                'tig' => [
+                        'tig_as' => 'C-16',
+                        'tig' => '24',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48f2',
+            ],
+            [
+                'grade' => 'C-12',
+                'tig' => [
+                        'tig_as' => 'C-11',
+                        'tig' => '18',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48e8',
+            ],
+            [
+                'grade' => 'E-2',
+                'tig' => [
+                        'tig_as' => 'E-1',
+                        'tig' => '3',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48a2',
+            ],
+            [
+                'grade' => 'F-2',
+                'tig' => [
+                        'tig_as' => 'F-1',
+                        'tig' => '24',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48ce',
+            ],
+            [
+                'grade' => 'WO-1',
+                'tig' => [
+                        'tig_as' => 'E-6',
+                        'tig' => '6',
+                ],
+                'updated_at' => '2015-09-17 01:31:44',
+                'created_at' => '2015-09-17 01:31:44',
+                'id' => '55fa1800e4bed82e078b48b4',
+            ],
+        ];
+
+        foreach ($tigs as $tig) {
+            $rec = new Tig($tig);
+            $rec->created_at = $tig['created_at'];
+            $rec->updated_at = $tig['updated_at'];
+            if (isset($tig['deleted'])) {
+                $rec->deleted_at = $tig['deleted_at'];
+            }
+            $rec->timestamps = false;
+            $rec->save();
+        }
+    }
+}
