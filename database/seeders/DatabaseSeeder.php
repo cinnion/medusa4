56a546337cf97dee25187c1b03e122d88eeab184
Douglas Needham <douglas.w.needham@gmail.com>
2025-09-04 19:29:18 -0400

    Seed testing from the production exam list.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,6 +16,7 @@ class DatabaseSeeder extends Seeder
             AwardSeeder::class,
             BranchSeeder::class,
             ChapterSeeder::class,
+            ExamListSeeder::class,
             GradeSeeder::class,
             MedusaConfigSeeder::class,
             RatingSeeder::class,
