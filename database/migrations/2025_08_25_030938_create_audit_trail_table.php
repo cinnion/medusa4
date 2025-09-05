f04901d5b422a44823bc92054af5bfa647f628b8
Douglas Needham <douglas.w.needham@gmail.com>
2025-09-05 11:44:40 -0400

    Fix the audit_trail migration
    - Remove conditional creation logic
    - Fix table name.

--- database/migrations/2025_08_25_030938_create_audit_trail_table.php
+++ database/migrations/2025_08_25_030938_create_audit_trail_table.php
@@ -11,60 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        if (!Schema::hasTable('audits')) {
-            Schema::create('audits', function (Blueprint $table) {
-                $table->id();
-                $table->timestamps();
+        Schema::create('audit_trail', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
 
-                $table->string('member_id');
-                $table->string('action');
-                $table->string('collection_name');
-                $table->string('document_id');
-                $table->json('document_values');
-                $table->string('from_where')->nullable();
-            });
-        } else {
-            echo "Table 'audits' already exists. Skipping creation." . PHP_EOL;
-        }
-
-//        Schema::collection('your_collection_name', function (Blueprint $collection) {
-//            $collection->command('collMod', [
-//                'validator' => [
-//                    '$jsonSchema' => [
-//                        'bsonType' => 'object',
-//                        'required' => ['member_id', 'action', 'collection_name', 'document_id', 'document_values'],
-//                        'properties' => [
-//                            'member_id' => [
-//                                'bsonType' => 'string',
-//                                'description' => 'must be a string and is required'
-//                            ],
-//                            'action' => [
-//                                'bsonType' => 'string',
-//                                'description' => 'must be a string and is required'
-//                            ],
-//                            'collection_name' => [
-//                                'bsonType' => 'string',
-//                                'description' => 'must be a string and is required'
-//                            ],
-//                            'document_id' => [
-//                                'bsonType' => 'string',
-//                                'description' => 'must be a string and is required'
-//                            ],
-//                            'document_values' => [
-//                                'bsonType' => 'object',
-//                                'description' => 'must be an object and is required'
-//                            ],
-//                            'from_where' => [
-//                                'bsonType' => ['string', 'null'],
-//                                'description' => 'can be a string or null'
-//                            ]
-//                        ]
-//                    ]
-//                ],
-//                'validationLevel' => 'moderate',
-//                'validationAction' => 'warn'
-//            ]);
-//        });
+            $table->string('member_id');
+            $table->string('action');
+            $table->string('collection_name');
+            $table->string('document_id');
+            $table->json('document_values');
+            $table->string('from_where')->nullable();
+        });
     }
 
     /**
@@ -72,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('audits');
+        Schema::dropIfExists('audit_trail');
     }
 };
